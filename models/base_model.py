2d4cdff6d9abeb097bcc538bdbd63fe5552d7fb1
DavidI <kamsidavid2@gmail.com>
2023-11-07 22:17:28 +0100

    write skeleton of code for BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""This module defines a BaseModel class"""
+
+
+class BaseModel:
+    """This class defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        id = 
+        created_at = 
+        updated_at = 
+
+
+    def save(self):
+        """Update the public instance attribute updated_at with the current datetime
+        """
+        pass
+
+
+    def to_dict(self):
+        """return a dictionary containing all keys/values of __dict__ of the instance
+        """
+        pass
