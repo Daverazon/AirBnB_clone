321860ef11407d613412bc147fa0c091934875d4
DavidI <kamsidavid2@gmail.com>
2023-11-08 19:36:01 +0100

    fix errors raised by pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,14 @@ class BaseModel:
     """This class defines all common attributes/methods for other classes
     """
     def __init__(self):
-        self.id = str(uuid.uuid4()) #generate unique id for each instance
-        self.created_at = datetime.now() #assign with the current datetime when an instance is created
-        self.updated_at = self.created_at #assign with the current datetime when an instance is created and
+        """Initialise a BaseModel object
+        """
+        self.id = str(uuid.uuid4())
+        # generate unique id for each instance
+        self.created_at = datetime.now()
+        # assign with the current datetime when an instance is created
+        self.updated_at = self.created_at
+        # assign with the current datetime when an instance is created and
         # it will be updated every time you change your object
 
     def __str__(self):
@@ -19,16 +24,21 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the public instance attribute updated_at with the current datetime
+        """Update the public instance attribute updated_at
+        with the current datetime
         """
         self.updated_at = datetime.now()
 
-
     def to_dict(self):
-        """return a dictionary containing all keys/values of __dict__ of the instance
+        """return a dictionary containing all keys/values of __dict__
+        of the instance
         """
-        json_representation = self.__dict__ #create dictionary containing all instance attributes
-        json_representation['__class__'] = self.__class__.__name__ #add class name to dictionary
-        self.created_at = datetime.isoformat(self.created_at) #convert to string object in isoformat
-        self.updated_at = datetime.isoformat(self.updated_at) #convert to string object in isoformat
+        json_representation = self.__dict__
+        # create dictionary containing all instance attributes
+        json_representation['__class__'] = self.__class__.__name__
+        # add class name to dictionary
+        self.created_at = datetime.isoformat(self.created_at)
+        # convert to string object in isoformat
+        self.updated_at = datetime.isoformat(self.updated_at)
+        # convert to string object in isoformat
         return json_representation
