129ccb3f7bca42794d859eecf7f178a29da0a518
DavidI <kamsidavid2@gmail.com>
2023-11-10 22:05:31 +0100

    add unfinished code for methods in task 7

--- console.py
+++ console.py
@@ -3,10 +3,77 @@
 """
 import cmd
 
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
         """Define a custom command interpreter for Airbnb clone"""
         prompt = "(hbnb) "
+
+        def do_create(self, cls):
+                """Creates a new instance of BaseModel,
+                saves it (to the JSON file) and prints the id"""
+                if cls == "":
+                        print("** class name missing **")
+                try:
+                        obj = eval(cls)()
+                        # create new instance and add it to FileStorage.__objects
+                except NameError:
+                        print("** class doesn't exist **")
+                else:
+                        storage.save()
+                        '''serialize the updated FileStorage.__objects
+                        dictionary and save to the json file'''
+
+        def help_create(self):
+                """Help text for create command"""
+                print("create <class_name>")
+                print("Creates a new instance of BaseModel,",
+                      "saves it (to the JSON file) and prints the id\n")
+
+        def do_show(self, clss):
+                """Prints the string representation of an instance based
+                  on the class name and id"""
+                
+
+        def help_show(self):
+                """Help text for show command"""
+                print("show <class_name> <id>")
+                print("Prints the string representation of an",
+                      " instance based on the class name and id\n")
+        
+        def do_destroy(self, ):
+                """Deletes an instance based on the class name
+                and id (save the change into the JSON file)"""
+
+
+        def help_destroy(self):
+                """Help text for destroy command"""
+                print("destroy <class_name> <id>")
+                print("Deletes an instance based on the class name",
+                       " and id (save the change into the JSON file)\n")
+
+        def do_all(self, ):
+                """ Prints all string representation of 
+                all instances based or not on the class name"""
+
+        def help_all(self):
+                """Help text for all command"""
+                print("all <class_name> or all")
+                print(" Prints all string representation of all",
+                      " instances based or not on the class name\n")
+
+        def do_update(self, ):
+                """Updates an instance based on the class name and id by adding
+                  or updating attribute (save the change into the JSON file)"""
+
+        def help_update(self):
+                """Help text for update command"""
+                print('update <class name> <id> <attribute name> "<attribute value>"')
+                print("Updates an instance based on the class name and id by",
+                      " adding or updating attribute (save the change into the JSON file)\n")
+
         def do_quit(self, line):
                 """quit + ENTER exits the program"""
                 return True
