ff3a03ec25ea33c60a4222ca247a3c9e18473fde
DavidI <kamsidavid2@gmail.com>
2023-11-11 16:01:25 +0100

    store changes in remote

--- console.py
+++ console.py
@@ -16,18 +16,20 @@ class HBNBCommand(cmd.Cmd):
                 arg = arg.split()
                 if len(arg) == 0:  # []
                         print("** class name missing **")
-                        self.cmdloop()
+                        return
                 # ["<class_name>"]
                 try:
                         cls = eval(arg[0]) # check if the class object exists
                 except NameError:
                         print("** class doesn't exist **")
-                        self.cmdloop()
+                        return
                 return cls
 
         def do_create(self, arg):
                 """Creates a new instance of BaseModel,
                 saves it (to the JSON file) and prints the id"""
+                if self.checkClass(arg) == None:
+                        return
                 self.checkClass(arg)()
                  # create new instance and add it to FileStorage.__objects
                 storage.save()
@@ -46,20 +48,24 @@ class HBNBCommand(cmd.Cmd):
                 args = args.split()
                 if len(args) == 1:  # ["<class_name>"]
                         print("** instance id missing **")
-                        self.cmdloop()
+                        return
                 # ["<class_name>", "<id>", ...]
                 id = args[1]
                 key = f"{cls.__name__}.{id}"
                 # create key for locating instance in storage
                 if key not in storage.all().keys():
                         print("** no instance found **")
-                        self.cmdloop()
+                        return
                 return(id, key)
 
         def do_show(self, args):
                 """Prints the string representation of an instance based
                   on the class name and id""" # [<class_name>, <id>]
+                if self.checkClass(args) == None:
+                        return
                 cls = self.checkClass(args)
+                if self.checkId(cls, args) == None:
+                        return
                 key = self.checkId(cls, args)[1]
                 dict_rep = storage.all()[key]
                 obj = cls(**dict_rep)  # recreate the object
@@ -82,19 +88,25 @@ class HBNBCommand(cmd.Cmd):
                 print("Deletes an instance based on the class name",
                        " and id (save the change into the JSON file)\n")
 
-        def do_all(self, cls):
+        def do_all(self, arg):
                 """ Prints all string representation of 
                 all instances based or not on the class name"""
-                if cls != "":
-                        try:
-                                cls = eval(cls)  # check if the class object exists
-                        except NameError:
-                                print("** class doesn't exist **")
-                                return  # show prompt again
-                        else:
-                                pass
-                else:
-                        pass
+                objects = storage.all()
+                if arg != "":
+                        # create dict with only objects of specified class
+                        if self.checkClass(arg) == None:
+                                return
+                        cls = self.checkClass(arg)
+                        objects = {key: dict_rep for key, dict_rep in objects.items() if key.startswith(f'{cls.__name__}')}
+                # no specified class so use dict with all stored objects
+                str_rep = []
+                for key, dict_rep in objects.items():
+                        cls = key.split(sep='.')[0]  # ['<class_name>', '<id>']
+                        obj = eval(cls)(dict_rep)  # recreate object
+                        str_rep.append(obj.__str__)  # add string rep to list
+                print(str_rep)
+
+                
 
         def help_all(self):
                 """Help text for all command"""
